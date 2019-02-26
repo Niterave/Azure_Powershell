0e82173810a10849f5a9155a0761548132b84306
Niterave <matt.richardson@moxieusa.com>
2019-02-26 15:34:14 -0500

    Updated Template
    Made several improvements and made it easier to read.

--- Add-Edit-NSG-Rules.ps1
+++ Add-Edit-NSG-Rules.ps1
@@ -1,61 +1,59 @@
-﻿# Network Security Group (NSG) Rule(s) Add/Edit template
-# Taken from https://docs.microsoft.com/en-us/azure/virtual-network/virtual-network-manage-nsg-arm-ps
-# Composed by Matt Richardson | Manager of Cloud Services | Moxie | matt.richardson@moxieusa.com
-# 
-# Pre-requisites: An understanding of NSG Inbound/Outbound rules and how they work:
-# https://docs.microsoft.com/en-us/azure/virtual-network/virtual-networks-nsg
-#
-# As with all of my scripts, I try to write them so all the user of the script needs to do is fill in the
-# variables and then run the script with no further edits needed.  Unfortunately in this case, each rule
-# must be individually defined, so simply filling in the variables isn't enough.
-
 # Log in to Azure.  This is only needed if you're using local powershell. This is not needed if you're
 # simply pasting this script into the Azure Portal Cloud Shell console, which is recommended.
-Login-AzureRmAccount
+# Login-AzureRmAccount
 
-#Define Variables. Variables must be surrounded with quotes.
-$SubscriptionName = "Subscription Name Here"
-$ResourceGroup = "Resource Group Name Here"
-$nsgName = "NSG Name Here"
+#Define Variables. surround with quotes even if there are no spaces.
+$SubscriptionName = "<Name of the Subscription>"
+$ResourceGroup = "<Resource Group that the NSG is in>"
+$nsgName = "<Applicable NSG assigned to appropriate network tier>"
 
-# Rule Variables
-
-# This is the Source IP address or subnet.  $LocalIPName is what the name will be.  Usually both values are the
-# same, however if your destination is any, you have to use an IP of an asterisk (*) and name it "any".  The $LocalIPName
-# will be used to name the rule.
-$LocalIP = "xxx.xxx.xxx.xxx/32"
-$LocalIPName = "Meaningful_Name"
+# This is the Source IP address or subnet.  $SouceIPName is what the name will be.  Usually both values are the
+# same, however if your destination is any, you have to use an IP of an asterisk (*) and name it "any".  The IP 
+# can also be a range, or a subnet.  i.e. 172.25.165.10-172.25.165.15 or 172.25.165.0/24.  For multiple comma 
+# separated IPs, you will have to add those via the portal web interface after the rule has been created.
+$LocalIP = "<Source IP>/32"
+$LocalIPName = "<Friendly Name for above IP>"
 
 # This is the Destination IP address or subnet.  $DestinationIPName is what the name will be.  Usually both values are the
 # same, however if your destination is any, you have to use an IP of an asterisk (*) and name it "any".
-$RemoteIP = "xxx.xxx.xxx.xxx/32"
-$RemoteIPName = "Meaningful_Name"
+$RemoteIP = "<Destination IP>/32"
+$RemoteIPName = "<Friendly Name for above IP>"
 
-# Port Number and port name.  Usually both values are the same, however if your Remote is any, you have to use 
-# an port of an asterisk (*) and name it "any".
-$Port = "587"
-$PortName = "SMTP"
+# Port Number and port name.  Usually both values are the same, however if your Remote is any, you have to use
+# a port of an asterisk (*) and name it "any".
+# Additional File Sharing Port: 445
+$Port = "<Applicable Port Number>"
+$PortName = "<Applicable port name, i.e. SSH, SFTP, RDP, LDAP, etc. If multiple ports, a meaningful name i.e. MAIL or FileShare>"
 
-# Protocol and protocol name.  Usually both values are the same, however if your Remote is any, you have to use 
+# Protocol and protocol name.  Usually both values are the same, however if your Remote is any, you have to use
 # an protocol of an asterisk (*) and name it "any".
-$Protocol = "tcp"
-$ProtocolName = "tcp"
+$Protocol = "<tcp> <udp> or <*>"
+$ProtocolName = "<tcp> <udp> or <any>"
 
-#Inbound and outbound rule numbers.  Making the numbers the same is encouraged.
-$InboundPriority = "1890"
-$OutboundPriority = "1890"
+#Inbound and outbound rule numbers.  Making the numbers the same is encouraged. This rule MUST be a higher
+#priority than the default Re:Sources rule that blocks all Virtual Network traffic.  i.e. the rules that 
+#are named like deny-virtualnetwork-any-inbound-tcp-all-virtualnetwork.  Typically these rules are numbers
+#4047-4050.
+$InboundPriority = "<Inbound Priority/Rule Number>"
+$OutboundPriority = "<Outbound Priority/Rule Number>"
 
 #Description used in both rules that will be created.
-$Description = "Meaningful_Description."
+$Description = "<Text discription of the rule.>"
+
+#########################################################################################################
+#
+# DO NOT EDIT ANTYHING BELOW - ALL VARIABLES HAVE BEEN DEFINED
+#
+#########################################################################################################
 
 # Connect to Subscription
 Select-AzureRmSubscription -SubscriptionName $SubscriptionName
 
-# Define NSG using variables provided above
+#Define NSG
 $nsg = Get-AzureRmNetworkSecurityGroup -ResourceGroupName $ResourceGroup -Name $nsgName
 
-# Use this template to add the desired rule(s). This example also shows how you can chain multiple rules
-# together.  Re:Sources NSG rule naming standard is:
+# Use this template to add the desired rule(s). This example also shows how you can chain multiple rules together.
+# NSG rule naming standard is:
 # <agency identifier>-<access>-<source>-<source port>-<direction>-<protocol>-<destination port>-<destination>
 # See example below
 Add-AzureRmNetworkSecurityRuleConfig -NetworkSecurityGroup $nsg `
@@ -71,7 +69,6 @@ Add-AzureRmNetworkSecurityRuleConfig -NetworkSecurityGroup $nsg `
 -DestinationPortRange $Port
 
 Add-AzureRmNetworkSecurityRuleConfig -NetworkSecurityGroup $nsg `
-Add-AzureRmNetworkSecurityRuleConfig -NetworkSecurityGroup $nsg `
 -Name mox-allow-$LocalIPName-any-outbound-$ProtocolName-$PortName-$RemoteIPName `
 -Description "$Description" `
 -Access Allow `
@@ -82,19 +79,6 @@ Add-AzureRmNetworkSecurityRuleConfig -NetworkSecurityGroup $nsg `
 -SourcePortRange * `
 -DestinationAddressPrefix $RemoteIP `
 -DestinationPortRange $Port
-
-#Use this template to edit an existing rule.  Very handy since you can't edit some fields in the portal.
-#Set-AzureRmNetworkSecurityRuleConfig -NetworkSecurityGroup $nsg `
-#-Name mox-allow-172.25.164.4-any-outbound-any-22-209.208.245.115 `
-#-Description "Allow SFTP outbound" `
-#-Access Allow `
-#-Protocol Tcp `
-#-Direction Outbound `
-#-Priority 3370 `
-#-SourceAddressPrefix 172.25.164.4/32 `
-#-SourcePortRange * `
-#-DestinationAddressPrefix 209.208.245.115 `
-#-DestinationPortRange 22
 
 # Apply the Rule
 Set-AzureRmNetworkSecurityGroup -NetworkSecurityGroup $nsg
